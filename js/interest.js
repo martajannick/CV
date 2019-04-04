faa4c797a3fb97f559e6c561d0c0afc1becf3bb0
Marta <martajanik12@gmail.com>
2019-04-04 23:04:22 +0200

    z koniem

---
+++ js/interest.js
@@ -0,0 +1,15 @@
+function TurnOn(){
+    document.getElementById('imgBike').src="../../photos/interest/bike2.jpg"
+    }
+    
+function TurnOff(){
+    document.getElementById('imgBike').src="../../photos/interest/bike1.jpg"
+    }
+
+function TurnOnT(){
+    document.getElementById('imgTyp').src="../../photos/interest/typiara2.jpg"
+    }
+
+function TurnOffT(){
+    document.getElementById('imgTyp').src="../../photos/interest/typiara1.jpg"
+    }
